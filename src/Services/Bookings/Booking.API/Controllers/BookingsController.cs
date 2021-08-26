e153c537d0398daee8e3ef6f90e756763d7c092f
qbuser <qbuser@QB-IPL-1680>
2021-08-26 15:37:52 +0530

    added mongo api and docker compose files

--- src/Services/Bookings/Booking.API/Controllers/BookingsController.cs
+++ src/Services/Bookings/Booking.API/Controllers/BookingsController.cs
@@ -2,9 +2,10 @@
 using Booking.API.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Booking.API.Controllers
@@ -14,17 +15,64 @@ namespace Booking.API.Controllers
     public class BookingsController : ControllerBase
     {
         private readonly IEventRepository _repository;
+        private readonly ILogger<BookingsController> _logger;
 
-        public BookingsController(IEventRepository repository)
+        public BookingsController(IEventRepository repository, ILogger<BookingsController> logger)
         {
             _repository = repository ?? throw new ArgumentNullException();
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Event>> GetEventsAsync()
+        [ProducesResponseType(typeof(IEnumerable<Event>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Event>>> GetProducts()
         {
-            var events = await _repository.GetEvents();
-            return events;
+            var products = await _repository.GetEvents();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "GetEvent")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Event), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Event>> GetProductById(string id)
+        {
+            var eventResult = await _repository.GetEvents(id);
+            if (eventResult == null)
+            {
+                return NotFound();
+            }
+            return Ok(eventResult);
+        }
+
+        [Route("[action]/{category}", Name = "GetEventByCategory")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Event>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Event>>> GetEventsByCategory(string category)
+        {
+            var products = await _repository.GetEventsByCategory(category);
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Event), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Event>> CreateEvent([FromBody] Event eventModel)
+        {
+            await _repository.CreateEvent(eventModel);
+            return CreatedAtRoute("GetEvent", new { id = eventModel.Id }, eventModel);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Event), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateEvent([FromBody] Event eventModel)
+        {
+            return Ok(await _repository.UpdateEvent(eventModel));
+        }
+
+        [HttpDelete("{id}", Name = "DeleteEvent")]
+        [ProducesResponseType(typeof(Event), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteEventById(string id)
+        {
+            return Ok(await _repository.DeleteEvent(id));
         }
     }
 }
